839f0cfda96c8ed182958335b4caa7a99a466f1b
XiaoPan <2489964425@qq.com>
2021-01-08 14:42:34 +0800

    初步实现添加站点功能

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
+$('.addWeb').on('click',()=>{
+  $('.dialog').removeClass('hiddenDialog')
+})
+const hidden = () => {
+  $('.dialog').addClass('hiddenDialog')
+}
+let title,url,logoUrl
+$('.webTitle').on('blur',(e) => {
+  title = e.target.value
+})
+$('.webUrl').on('blur',(e) => {
+  url = e.target.value
+  logoUrl = url[0].toUpperCase()
+})
+$('.determineBtn').on('click',()=>{
+  url = 'https://' + url
+  hidden()
+  let li = $(`
+    <li>
+      <a href="${url}">
+        <div class="site">
+          <div class="logo">${logoUrl}</div>
+          <div class="link">${title}</div>
+        </div>
+      </a>
+    </li>
+  `).appendTo($('.siteList'))
+})
+
 
