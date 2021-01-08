2b86c8618ada3f30a288e19feb8518c0d246c0ee
XiaoPan <2489964425@qq.com>
2021-01-08 14:58:46 +0800

    进一步完成添加站点功能

--- src/main.js
+++ src/main.js
@@ -6,20 +6,23 @@ const hidden = () => {
 }
 let title,url,logoUrl
 $('.webTitle').on('blur',(e) => {
-  title = e.target.value
+  title = (e.target.value).trim()
 })
 $('.webUrl').on('blur',(e) => {
-  url = e.target.value
-  logoUrl = url[0].toUpperCase()
+  url = (e.target.value).trim()
+  logoUrl = url
 })
 $('.determineBtn').on('click',()=>{
+  if(title === undefined || title === '') {
+    title = logoUrl
+  }
   url = 'https://' + url
   hidden()
   let li = $(`
     <li>
       <a href="${url}">
         <div class="site">
-          <div class="logo">${logoUrl}</div>
+          <div class="logo">${logoUrl[0].toUpperCase()}</div>
           <div class="link">${title}</div>
         </div>
       </a>
