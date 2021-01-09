346c75d729e5d62f09942bf00e1f116c0539357f
XiaoPan <2489964425@qq.com>
2021-01-09 18:02:50 +0800

    利用 hash 数据结构存储数据，动态展示

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
+let $siteList = $('.siteList')
+const hashMap = [
+  {logo:'./images/github.jpg',url:'https://github.com/',title:'Github',logoType:'image'},
+  {logo:'./images/bilibili.jpg',url:'https://bilibili.com/',title:'哔哩哔哩',logoType:'image'},
+  {logo:'./images/acfun.png',url:'https://www.acfun.cn/',title:'AcFun',logoType:'image'},
+  {logo:'Z',url:'https://zhihu.com/follow',title:'知乎',logoType:'text'}
+]
+
+const render = () => {
+  hashMap.forEach(node => {
+    const $li = $(`
+      <li>
+        <a href="${node.url}">
+          <div class="site">
+            <div class="logo">
+              <img src="${node.logo}" alt="" />
+            </div>
+            <div class="link">${node.title}</div>
+          </div>
+        </a>
+      </li>
+    `).appendTo($siteList)
+  })
+}
+render()
+
+
+
+
+
+
+
 $('.addWeb').on('click',()=>{
   $('.dialog').removeClass('hiddenDialog')
 })
@@ -12,22 +44,30 @@ $('.webUrl').on('blur',(e) => {
   url = (e.target.value).trim()
   logoUrl = url
 })
+
 $('.determineBtn').on('click',()=>{
+  $siteList.find('li').remove()
   if(title === undefined || title === '') {
     title = logoUrl
   }
   url = 'https://' + url
   hidden()
-  let li = $(`
-    <li>
-      <a href="${url}">
-        <div class="site">
-          <div class="logo">${logoUrl[0].toUpperCase()}</div>
-          <div class="link">${title}</div>
-        </div>
-      </a>
-    </li>
-  `).appendTo($('.siteList'))
+  hashMap.push({
+    logo:logoUrl[0].toUpperCase(),
+    url:url,
+    title:title,
+    logoType:'text'
+  })
+  hashMap.forEach(node => {
+    const $li = $(`
+      <li>
+        <a href="${node.url}">
+          <div class="site">
+            <div class="logo">${node.url[8].toUpperCase()}</div>
+            <div class="link">${node.title}</div>
+          </div>
+        </a>
+      </li>
+    `).appendTo($siteList)
+  })
 })
-
-
